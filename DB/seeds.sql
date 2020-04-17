5d7a0505f477ed6eed481d3b47803b646750516d
Anthony Arroyo <anthony.aarroy2@gmail.com>
2020-04-17 00:01:48 -0500

    finished app

---
+++ DB/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("beyond burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("impossible burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("black-bean burger", false);
